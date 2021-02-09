b9516f4de73cf659ab384efd380f1e336414405b
gjj2828 <gjj2828@gmail.com>
2021-02-09 23:58:54 +0800

    上传示例

---
+++ Assets/Editor/BundleTool.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BundleTool : MonoBehaviour
+{
+    [MenuItem("Tool/Bundle")]
+    public static void Bundle()
+    {
+        var names = AssetDatabase.GetAllAssetBundleNames();
+        var builds = new List<AssetBundleBuild>();
+        foreach(string name in names)
+        {
+            var assetPaths = AssetDatabase.GetAssetPathsFromAssetBundle(name);
+            if(assetPaths == null || assetPaths.Length <= 0) continue;
+            string[] nv = name.Split('.');
+            var build = new AssetBundleBuild() { assetBundleName = nv[0], assetNames = assetPaths };
+            if(nv.Length > 1)
+            {
+                build.assetBundleVariant = nv[1];
+            }
+            builds.Add(build);
+        }
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, builds.ToArray()
+            , BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle
+            , BuildTarget.Android);
+        //BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath
+        //    , BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.DeterministicAssetBundle
+        //    , BuildTarget.Android);
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public enum EVariant
+{
+    en,
+    cn
+}
+
+public class Launcher : MonoBehaviour
+{
+    public Transform m_UIParent;
+    public EVariant m_Variant;
+
+    private static string m_ABNameUI = "ui";
+    private static string m_ABNameBG = "bg";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var bundleBG = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, m_ABNameBG + "." + m_Variant.ToString()));
+        bundleBG.LoadAsset<Sprite>("bg");
+        var bundleUI = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, m_ABNameUI));
+        var assetBG = bundleUI.LoadAsset<GameObject>("BG");
+        var bg = Instantiate(assetBG);
+        bg.transform.parent = m_UIParent;
+        bg.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
